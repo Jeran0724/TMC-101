4c34a242e6aa02504b07a7151c868c0df246f7eb
Jeranny <jeranminaya@gmail.com>
2024-09-12 13:34:51 -0400

    Programas pt.1

---
+++ Prácticas/Programa 4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* Superficie del Triángulo.
+El rpograma, al recibir como datos la base
+y la altura de un tria'ngulo, calcula su
+superficie.
+
+BAS, ALT Y SUP: Variables de Tipo Real. */
+
+void main (void)
+
+{
+    float BAS, ALT, SUP;
+    printf ("Ingrese la Base y la Altura del Triángulo: ");
+    scanf ("%f %f", &BAS, %ALT);
+    SUP = BAS * ALT / 2;
+    printf ("\nLa Superficie del Triángulo es: %5.2f", SUP);
+    }
